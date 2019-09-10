549c16226a2c42ce4ebde06bae165cdd86ad270d
Olga804 <54376627+Olga804@users.noreply.github.com>
2019-09-10 15:05:13 +0300

    Update MainPage.java

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,30 +1,81 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.interactions.SendKeysAction;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
+import java.util.function.Function;
 
-public class MainPage extends BasePage {
-    @FindBy(xpath = "//button[contains(@aria-label,'Ипотека')]")
-    private WebElement searchTextField;
+public class CreditPage  {
+
+    By estateCost = By.xpath("//input[@id='estateCost']");
+    By initialFee = By.xpath("//input[@id='initialFee']");
+    By creditTerm = By.xpath("//input[@id='creditTerm']");
+    By paidToCard = By.xpath("//input[@data-test-id='paidToCard']");
+    By canConfirmIncome = By.xpath("//input[@data-test-id='canConfirmIncome']");
+    By youngFamilyDiscount = By.xpath("//input[@data-test-id='youngFamilyDiscount']");
+    /*  By amountOfCredit = By.xpath("//div[@data-test-id = amountOfCredit]");
+    By mounthlyPayment = By.xpath("//div[@data-test-id = mounthlyPayment]");
+    By requiredIncome = By.xpath("//div[@data-test-id = requiredIncome]");
+    By rate = By.xpath("//div[@data-test-id = rate]");
+
+    */
+
+   WebDriver driver;
+
+    public CreditPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void writeForm(String fullPrice, String  pay, String period){
+        driver.switchTo().frame("iFrameResizer0");
+         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",driver.findElement(estateCost) );
+        driver.findElement(estateCost).clear();
+        driver.findElement(estateCost).sendKeys(fullPrice);
+        waitingChenge(initialFee);
+        driver.findElement(initialFee).clear();
+        driver.findElement(initialFee).sendKeys(pay);
+        driver.findElement(creditTerm).clear();
+        driver.findElement(creditTerm).sendKeys(period);
 
 
-    @FindBy(xpath = "//a[contains(text(),'Ипотека на готовое')]")
-    WebElement searchButton;
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",driver.findElement(creditTerm) );
 
-
-    public void search(){
-        Actions actions = new Actions(driver);
         Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
-        wait.until(ExpectedConditions.visibilityOf(searchTextField));
-        actions.moveToElement(searchTextField).build().perform();
-        wait.until(ExpectedConditions.visibilityOf(searchButton));
-        searchButton.click();
+        //wait.until(ExpectedConditions.visibilityOf(driver.findElement(paidToCard)));
+        if(driver.findElement(paidToCard).isSelected()){
+           driver.findElement(paidToCard).click();
+        }
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(canConfirmIncome)));
+        if(!driver.findElement(canConfirmIncome).isSelected()){
+            driver.findElement(canConfirmIncome).click();
+        }
+        if(!driver.findElement(youngFamilyDiscount).isSelected()){
+            driver.findElement(youngFamilyDiscount).click();
+        }
 
     }
+
+
+    public void waitingChenge(By by){
+        String oldValue = driver.findElement(by).getAttribute("value");
+        Function<? super WebDriver, Object> valueChanged = new ExpectedCondition<Object>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                return !webDriver.findElement(by).getAttribute("value").equals(oldValue);
+            }
+        };
+        //действие для изменения значения
+        WebDriverWait wait = new WebDriverWait(driver, 5000);
+        wait.until(valueChanged);
+    }
+
 
 }
